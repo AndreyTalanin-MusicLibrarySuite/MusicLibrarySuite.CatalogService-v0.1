701463e35f6930655147b0d328c745e29987c7ea
Andrey Talanin <andrey.talanin@outlook.com>
2022-10-11 21:10:59 +0300

    Add the SqlServerCatalogServiceDbContext database context class

---
+++ Source/MusicLibrarySuite.CatalogService.Data.SqlServer/Contexts/SqlServerCatalogServiceDbContext.cs
@@ -0,0 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+
+using MusicLibrarySuite.CatalogService.Data.Contexts;
+
+namespace MusicLibrarySuite.CatalogService.Data.SqlServer.Contexts;
+
+/// <summary>
+/// Represents a SQL Server - specific implementation of the catalog service database context.
+/// </summary>
+public class SqlServerCatalogServiceDbContext : CatalogServiceDbContext
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SqlServerCatalogServiceDbContext" /> type.
+    /// </summary>
+    public SqlServerCatalogServiceDbContext()
+        : base()
+    {
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="SqlServerCatalogServiceDbContext" /> type using the specified database context options.
+    /// </summary>
+    /// <param name="contextOptions">The database context options.</param>
+    public SqlServerCatalogServiceDbContext(DbContextOptions contextOptions)
+        : base(contextOptions)
+    {
+    }
+}
