309bc0ae7df35529884f69dea7e585cf0a58c769
Andrey Talanin <andrey.talanin@outlook.com>
2022-10-11 21:12:41 +0300

    Add the InitialMigration database migration class

---
+++ Source/MusicLibrarySuite.CatalogService.Data.SqlServer/Migrations/20220926203813_InitialMigration.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MusicLibrarySuite.CatalogService.Data.SqlServer.Migrations;
+
+/// <summary>
+/// Represents an empty initial database migration.
+/// </summary>
+public partial class InitialMigration : Migration
+{
+    /// <inheritdoc />
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+    }
+
+    /// <inheritdoc />
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+    }
+}
