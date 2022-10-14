c81e53a2435fafd5d5955b45c921f31bc671fe86
Andrey Talanin <andrey.talanin@outlook.com>
2022-10-14 21:05:55 +0300

    Add the GenreDto database-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Data/Entities/GenreDto.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicLibrarySuite.CatalogService.Data.Entities;
+
+/// <summary>
+/// Represents a database model and a data-transfer object for a genre.
+/// </summary>
+public class GenreDto
+{
+    /// <summary>
+    /// Gets or sets the genre's unique identifier.
+    /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the genre's name.
+    /// </summary>
+    [StringLength(256)]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the genre's description.
+    /// </summary>
+    [StringLength(2048)]
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the current genre is a system entity like "Unknown Genre".
+    /// </summary>
+    public bool SystemEntity { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the current genre is enabled.
+    /// </summary>
+    public bool Enabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value representing the moment of time when the entity was initially created.
+    /// </summary>
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTimeOffset CreatedOn { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value representing the moment of time when the entity was updated the last time.
+    /// </summary>
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTimeOffset UpdatedOn { get; set; }
+}
