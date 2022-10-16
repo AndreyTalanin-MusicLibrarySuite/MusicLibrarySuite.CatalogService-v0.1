88e27e1d552e5ca07d7cb65dc63c213a7ab19618
Andrey Talanin <andrey.talanin@outlook.com>
2022-10-16 22:01:29 +0300

    Add the GenreRelationshipDto database-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Data/Entities/GenreRelationshipDto.cs
@@ -0,0 +1,37 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibrarySuite.CatalogService.Data.Entities;
+
+/// <summary>
+/// Represents a database model and a data transfer object for a genre-to-genre relationship.
+/// </summary>
+public class GenreRelationshipDto
+{
+    /// <summary>
+    /// Gets or sets the principal genre's unique identifier.
+    /// </summary>
+    public Guid GenreId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the dependent genre's unique identifier.
+    /// </summary>
+    public Guid DependentGenreId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's name.
+    /// </summary>
+    [StringLength(256)]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the relationship's description.
+    /// </summary>
+    [StringLength(2048)]
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's display order.
+    /// </summary>
+    public int Order { get; set; }
+}
