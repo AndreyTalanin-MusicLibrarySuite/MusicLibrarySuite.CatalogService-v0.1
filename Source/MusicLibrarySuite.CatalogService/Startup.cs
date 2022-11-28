fd1977da50635a9bc39201c0b291dd247f6e6568
Andrey Talanin <andrey.talanin@outlook.com>
2022-11-28 19:14:44 +0300

    Update the OpenAPI document version

--- Source/MusicLibrarySuite.CatalogService/Startup.cs
+++ Source/MusicLibrarySuite.CatalogService/Startup.cs
@@ -94,9 +94,9 @@ public class Startup
 
             options.SwaggerDoc("MusicLibrarySuite.CatalogService", new OpenApiInfo()
             {
-                Title = "Music Library Suite - Catalog Service API v0.3.0-rc1",
+                Title = "Music Library Suite - Catalog Service API v0.3.0-rc2",
                 Description = "Initial pre-release (unstable) API version.",
-                Version = "v0.3.0-rc1",
+                Version = "v0.3.0-rc2",
                 Contact = contact,
                 License = license,
             });
