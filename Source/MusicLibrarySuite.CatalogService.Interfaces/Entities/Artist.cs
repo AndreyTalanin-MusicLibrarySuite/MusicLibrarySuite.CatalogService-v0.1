9cf846642d0c28e576531ef09f65389e688212a7
Andrey Talanin <andrey.talanin@outlook.com>
2022-12-11 17:07:22 +0300

    Add the Artist application-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/Artist.cs
@@ -0,0 +1,54 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibrarySuite.CatalogService.Interfaces.Entities;
+
+/// <summary>
+/// Represents an artist.
+/// </summary>
+public class Artist
+{
+    /// <summary>
+    /// Gets or sets the artist's unique identifier.
+    /// </summary>
+    [Required]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the artist's name.
+    /// </summary>
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the artist's description.
+    /// </summary>
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the disambiguation text in case multiple artists have the same name.
+    /// </summary>
+    public string? DisambiguationText { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the current artist is a system entity like "Unknown Artist".
+    /// </summary>
+    [Required]
+    public bool SystemEntity { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the current artist is enabled.
+    /// </summary>
+    [Required]
+    public bool Enabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value representing the moment of time when the entity was initially created.
+    /// </summary>
+    public DateTimeOffset CreatedOn { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value representing the moment of time when the entity was updated the last time.
+    /// </summary>
+    public DateTimeOffset UpdatedOn { get; set; }
+}
