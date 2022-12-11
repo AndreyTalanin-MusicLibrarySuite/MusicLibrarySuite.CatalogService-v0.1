7bc66b680b26c750907347caa7613db903ddc308
Andrey Talanin <andrey.talanin@outlook.com>
2022-12-11 17:37:12 +0300

    Implement the ArtistService service class

--- Source/MusicLibrarySuite.CatalogService.Core/Services/ArtistService.cs
+++ Source/MusicLibrarySuite.CatalogService.Core/Services/ArtistService.cs
@@ -2,7 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
+using AutoMapper;
+
 using MusicLibrarySuite.CatalogService.Core.Services.Abstractions;
+using MusicLibrarySuite.CatalogService.Data.Entities;
+using MusicLibrarySuite.CatalogService.Data.Entities.Base;
+using MusicLibrarySuite.CatalogService.Data.Repositories.Abstractions;
 using MusicLibrarySuite.CatalogService.Interfaces.Entities;
 
 namespace MusicLibrarySuite.CatalogService.Core.Services;
@@ -12,24 +17,77 @@ namespace MusicLibrarySuite.CatalogService.Core.Services;
 /// </summary>
 public class ArtistService : IArtistService
 {
-    /// <inheritdoc />
-    public Task<Artist?> GetArtistAsync(Guid artistId) => throw new NotImplementedException();
+    private readonly IArtistRepository m_artistRepository;
+    private readonly IMapper m_mapper;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ArtistService" /> type using the specified services.
+    /// </summary>
+    /// <param name="artistRepository">The artist repository.</param>
+    /// <param name="mapper">The AutoMapper mapper.</param>
+    public ArtistService(IArtistRepository artistRepository, IMapper mapper)
+    {
+        m_artistRepository = artistRepository;
+        m_mapper = mapper;
+    }
 
     /// <inheritdoc />
-    public Task<Artist[]> GetArtistsAsync() => throw new NotImplementedException();
+    public async Task<Artist?> GetArtistAsync(Guid artistId)
+    {
+        ArtistDto? artistDto = await m_artistRepository.GetArtistAsync(artistId);
+        Artist? artist = m_mapper.Map<Artist?>(artistDto);
+        return artist;
+    }
 
     /// <inheritdoc />
-    public Task<Artist[]> GetArtistsAsync(IEnumerable<Guid> artistIds) => throw new NotImplementedException();
+    public async Task<Artist[]> GetArtistsAsync()
+    {
+        ArtistDto[] artistDtoArray = await m_artistRepository.GetArtistsAsync();
+        Artist[] artistArray = m_mapper.Map<Artist[]>(artistDtoArray);
+        return artistArray;
+    }
 
     /// <inheritdoc />
-    public Task<ArtistPageResponse> GetArtistsAsync(ArtistRequest artistRequest) => throw new NotImplementedException();
+    public async Task<Artist[]> GetArtistsAsync(IEnumerable<Guid> artistIds)
+    {
+        ArtistDto[] artistDtoArray = await m_artistRepository.GetArtistsAsync(artistIds);
+        Artist[] artistArray = m_mapper.Map<Artist[]>(artistDtoArray);
+        return artistArray;
+    }
 
     /// <inheritdoc />
-    public Task<Artist> CreateArtistAsync(Artist artist) => throw new NotImplementedException();
+    public async Task<ArtistPageResponse> GetArtistsAsync(ArtistRequest artistRequest)
+    {
+        ArtistRequestDto artistRequestDto = m_mapper.Map<ArtistRequestDto>(artistRequest);
+        PageResponseDto<ArtistDto> pageResponseDto = await m_artistRepository.GetArtistsAsync(artistRequestDto);
+        ArtistPageResponse pageResponse = m_mapper.Map<ArtistPageResponse>(pageResponseDto);
+
+        pageResponse.CompletedOn = DateTimeOffset.Now;
+
+        return pageResponse;
+    }
 
     /// <inheritdoc />
-    public Task<bool> UpdateArtistAsync(Artist artist) => throw new NotImplementedException();
+    public async Task<Artist> CreateArtistAsync(Artist artist)
+    {
+        ArtistDto artistDto = m_mapper.Map<ArtistDto>(artist);
+        ArtistDto createdArtistDto = await m_artistRepository.CreateArtistAsync(artistDto);
+        Artist createdArtist = m_mapper.Map<Artist>(createdArtistDto);
+        return createdArtist;
+    }
 
     /// <inheritdoc />
-    public Task<bool> DeleteArtistAsync(Guid artistId) => throw new NotImplementedException();
+    public async Task<bool> UpdateArtistAsync(Artist artist)
+    {
+        ArtistDto artistDto = m_mapper.Map<ArtistDto>(artist);
+        var updated = await m_artistRepository.UpdateArtistAsync(artistDto);
+        return updated;
+    }
+
+    /// <inheritdoc />
+    public async Task<bool> DeleteArtistAsync(Guid artistId)
+    {
+        var deleted = await m_artistRepository.DeleteArtistAsync(artistId);
+        return deleted;
+    }
 }
