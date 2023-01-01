c004f07ee13d210429b5c2828c3efafa3ceb1de1
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-01 18:31:12 +0300

    Add the Work and Genre navigation properties to the WorkGenre application-level entity

--- Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/WorkGenre.cs
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/WorkGenre.cs
@@ -25,4 +25,16 @@ public class WorkGenre
     /// </summary>
     [Required]
     public int Order { get; set; }
+
+    /// <summary>
+    /// Gets or sets the work.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned from the database.</remarks>
+    public Work? Work { get; set; }
+
+    /// <summary>
+    /// Gets or sets the genre.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned from the database.</remarks>
+    public Genre? Genre { get; set; }
 }
