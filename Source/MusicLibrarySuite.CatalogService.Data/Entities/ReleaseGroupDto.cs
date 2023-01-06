67b8221de934be745d482b407751d50dcec3845e
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-06 18:22:34 +0300

    Add the ReleaseGroupDto database-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Data/Entities/ReleaseGroupDto.cs
@@ -0,0 +1,53 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MusicLibrarySuite.CatalogService.Data.Entities;
+
+/// <summary>
+/// Represents a database model and a data-transfer object for a release group.
+/// </summary>
+public class ReleaseGroupDto
+{
+    /// <summary>
+    /// Gets or sets the release group's unique identifier.
+    /// </summary>
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public Guid Id { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release group's title.
+    /// </summary>
+    [StringLength(256)]
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the release group's description.
+    /// </summary>
+    [StringLength(2048)]
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the disambiguation text in case multiple release groups have the same name.
+    /// </summary>
+    [StringLength(2048)]
+    public string? DisambiguationText { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the current release group is enabled.
+    /// </summary>
+    public bool Enabled { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value representing the moment of time when the entity was initially created.
+    /// </summary>
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTimeOffset CreatedOn { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value representing the moment of time when the entity was updated the last time.
+    /// </summary>
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public DateTimeOffset UpdatedOn { get; set; }
+}
