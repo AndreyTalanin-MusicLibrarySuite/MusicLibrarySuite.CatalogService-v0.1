778f05c42806e9f2adc18699826af2eee86b67b8
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-06 20:27:06 +0300

    Add the ReleaseGroupPageResponse application-level data-transfer class

---
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseGroupPageResponse.cs
@@ -0,0 +1,10 @@
+using MusicLibrarySuite.CatalogService.Interfaces.Entities.Base;
+
+namespace MusicLibrarySuite.CatalogService.Interfaces.Entities;
+
+/// <summary>
+/// Represents a non-generic wrapper for the <see cref="PageResponse{T}" /> class where <see cref="ReleaseGroup" /> is the entity type.
+/// </summary>
+public class ReleaseGroupPageResponse : PageResponse<ReleaseGroup>
+{
+}
