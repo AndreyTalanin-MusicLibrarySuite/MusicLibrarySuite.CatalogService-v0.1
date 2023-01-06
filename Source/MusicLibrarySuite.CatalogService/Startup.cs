98776d62b613b7e185071d72e0cf9483af5a6dfa
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-06 21:13:01 +0300

    Add services to the IoC container

--- Source/MusicLibrarySuite.CatalogService/Startup.cs
+++ Source/MusicLibrarySuite.CatalogService/Startup.cs
@@ -48,6 +48,7 @@ public class Startup
         {
             options.AddProfile<CommonDatabaseProfile>();
             options.AddProfile<ArtistDatabaseProfile>();
+            options.AddProfile<ReleaseGroupDatabaseProfile>();
             options.AddProfile<GenreDatabaseProfile>();
             options.AddProfile<ProductDatabaseProfile>();
             options.AddProfile<WorkDatabaseProfile>();
@@ -106,11 +107,13 @@ public class Startup
         });
 
         services.AddScoped<IArtistRepository, SqlServerArtistRepository>();
+        services.AddScoped<IReleaseGroupRepository, SqlServerReleaseGroupRepository>();
         services.AddScoped<IGenreRepository, SqlServerGenreRepository>();
         services.AddScoped<IProductRepository, SqlServerProductRepository>();
         services.AddScoped<IWorkRepository, SqlServerWorkRepository>();
 
         services.AddScoped<IArtistService, ArtistService>();
+        services.AddScoped<IReleaseGroupService, ReleaseGroupService>();
         services.AddScoped<IGenreService, GenreService>();
         services.AddScoped<IProductService, ProductService>();
         services.AddScoped<IWorkService, WorkService>();
