1d6bed6953fd8233379aad2e6afcdda99c03d69f
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-22 14:51:12 +0300

    Add the Release and DependentRelease navigation properties to the ReleaseRelationship application-level entity

--- Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseRelationship.cs
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseRelationship.cs
@@ -30,4 +30,16 @@ public class ReleaseRelationship
     /// Gets or sets the relationship's description.
     /// </summary>
     public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the principal release.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned by the application.</remarks>
+    public Release? Release { get; set; }
+
+    /// <summary>
+    /// Gets or sets the dependent release.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned by the application.</remarks>
+    public Release? DependentRelease { get; set; }
 }
