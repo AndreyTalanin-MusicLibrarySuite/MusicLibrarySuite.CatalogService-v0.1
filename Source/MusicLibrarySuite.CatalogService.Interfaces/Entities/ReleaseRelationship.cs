f6c7bddd8c0c5808b90db171eeb7ce715f53d240
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-22 14:48:32 +0300

    Add the ReleaseRelationship application-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseRelationship.cs
@@ -0,0 +1,33 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibrarySuite.CatalogService.Interfaces.Entities;
+
+/// <summary>
+/// Represents a release-to-release relationship.
+/// </summary>
+public class ReleaseRelationship
+{
+    /// <summary>
+    /// Gets or sets the principal release's unique identifier.
+    /// </summary>
+    [Required]
+    public Guid ReleaseId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the dependent release's unique identifier.
+    /// </summary>
+    [Required]
+    public Guid DependentReleaseId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's name.
+    /// </summary>
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the relationship's description.
+    /// </summary>
+    public string? Description { get; set; }
+}
