e3537c37468c38d21e3e13448c643a665433d770
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-24 01:36:00 +0300

    Add the ReleasePerformer application-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleasePerformer.cs
@@ -0,0 +1,22 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibrarySuite.CatalogService.Interfaces.Entities;
+
+/// <summary>
+/// Represents a release-to-artist relationship where the artist has the "performer" role.
+/// </summary>
+public class ReleasePerformer
+{
+    /// <summary>
+    /// Gets or sets the release's unique identifier.
+    /// </summary>
+    [Required]
+    public Guid ReleaseId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the artist's unique identifier.
+    /// </summary>
+    [Required]
+    public Guid ArtistId { get; set; }
+}
