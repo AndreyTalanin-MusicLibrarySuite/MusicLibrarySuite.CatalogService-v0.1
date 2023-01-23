1ca20b8cdf4ad31d67be2ebae5d710ecb66efdbc
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-23 23:43:53 +0300

    Add the ReleaseFeaturedArtistDto database-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Data/Entities/ReleaseFeaturedArtistDto.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MusicLibrarySuite.CatalogService.Data.Entities;
+
+/// <summary>
+/// Represents a database model and a data transfer object for a release-to-artist relationship where the artist has the "featured artist" role.
+/// </summary>
+public class ReleaseFeaturedArtistDto
+{
+    /// <summary>
+    /// Gets or sets the release's unique identifier.
+    /// </summary>
+    public Guid ReleaseId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the artist's unique identifier.
+    /// </summary>
+    public Guid ArtistId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's display order.
+    /// </summary>
+    public int Order { get; set; }
+}
