e84c9e7af6161a3c99626d6be8d9e0e5eeda6fc2
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-24 01:33:03 +0300

    Add the ReleaseFeaturedArtist application-level entity

--- Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/WorkFeaturedArtist.cs
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/WorkFeaturedArtist.cs
@@ -21,12 +21,6 @@ public class WorkFeaturedArtist
     public Guid ArtistId { get; set; }
 
     /// <summary>
-    /// Gets or sets the relationship's display order.
-    /// </summary>
-    [Required]
-    public int Order { get; set; }
-
-    /// <summary>
     /// Gets or sets the work.
     /// </summary>
     /// <remarks>This property is only used to store data returned from the database.</remarks>
