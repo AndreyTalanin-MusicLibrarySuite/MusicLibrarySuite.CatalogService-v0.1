98173c748e63a55a26bf64c77c0b56bf3ff4d20d
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-25 22:09:50 +0300

    Add the Release and Genre navigation properties to the ReleaseGenre application-level entity

--- Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseGenre.cs
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseGenre.cs
@@ -19,4 +19,16 @@ public class ReleaseGenre
     /// </summary>
     [Required]
     public Guid GenreId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned from the database.</remarks>
+    public Release? Release { get; set; }
+
+    /// <summary>
+    /// Gets or sets the genre.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned from the database.</remarks>
+    public Genre? Genre { get; set; }
 }
