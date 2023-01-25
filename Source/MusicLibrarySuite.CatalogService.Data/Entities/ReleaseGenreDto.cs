ac493f5d021cf8134a0531d06c3c3bf0e5529a02
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-25 21:29:53 +0300

    Add the ReleaseGenreDto database-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Data/Entities/ReleaseGenreDto.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace MusicLibrarySuite.CatalogService.Data.Entities;
+
+/// <summary>
+/// Represents a database model and a data transfer object for a release-to-genre relationship.
+/// </summary>
+public class ReleaseGenreDto
+{
+    /// <summary>
+    /// Gets or sets the release's unique identifier.
+    /// </summary>
+    public Guid ReleaseId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the genre's unique identifier.
+    /// </summary>
+    public Guid GenreId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's display order.
+    /// </summary>
+    public int Order { get; set; }
+}
