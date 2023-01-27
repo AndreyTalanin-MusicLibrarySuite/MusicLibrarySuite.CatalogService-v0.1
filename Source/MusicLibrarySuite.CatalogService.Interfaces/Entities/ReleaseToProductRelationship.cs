9cc4084757cbda6571dbb996d71d70518c6d515e
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-27 21:20:40 +0300

    Add the Release and Product navigation properties to the ReleaseToProductRelationship application-level entity

--- Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseToProductRelationship.cs
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseToProductRelationship.cs
@@ -30,4 +30,16 @@ public class ReleaseToProductRelationship
     /// Gets or sets the relationship's description.
     /// </summary>
     public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned from the database.</remarks>
+    public Release? Release { get; set; }
+
+    /// <summary>
+    /// Gets or sets the product.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned from the database.</remarks>
+    public Product? Product { get; set; }
 }
