16a9008f3a61f839aabea626643547359860fddb
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-28 21:49:34 +0300

    Add the Release and ReleaseGroup navigation properties to the ReleaseToReleaseGroupRelationship application-level entity

--- Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseToReleaseGroupRelationship.cs
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseToReleaseGroupRelationship.cs
@@ -30,4 +30,16 @@ public class ReleaseToReleaseGroupRelationship
     /// Gets or sets the relationship's description.
     /// </summary>
     public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned from the database.</remarks>
+    public Release? Release { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release group.
+    /// </summary>
+    /// <remarks>This property is only used to store data returned from the database.</remarks>
+    public ReleaseGroup? ReleaseGroup { get; set; }
 }
