88130417a89c5d43d8a6eb6252c9b82e8679a349
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-29 15:53:43 +0300

    Add the ReleaseTrackArtistDto database-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Data/Entities/ReleaseTrackArtistDto.cs
@@ -0,0 +1,34 @@
+using System;
+
+namespace MusicLibrarySuite.CatalogService.Data.Entities;
+
+/// <summary>
+/// Represents a database model and a data transfer object for a release-track-to-artist relationship.
+/// </summary>
+public class ReleaseTrackArtistDto
+{
+    /// <summary>
+    /// Gets or sets the release track's number.
+    /// </summary>
+    public byte TrackNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release media's number.
+    /// </summary>
+    public byte MediaNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release's unique identifier.
+    /// </summary>
+    public Guid ReleaseId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the artist's unique identifier.
+    /// </summary>
+    public Guid ArtistId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's display order.
+    /// </summary>
+    public int Order { get; set; }
+}
