db8d3fd72d6e2d99f3a82da8ccda0079e44d5467
Andrey Talanin <andrey.talanin@outlook.com>
2023-01-31 18:54:16 +0300

    Add the ReleaseTrackToProductRelationship application-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Interfaces/Entities/ReleaseTrackToProductRelationship.cs
@@ -0,0 +1,45 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibrarySuite.CatalogService.Interfaces.Entities;
+
+/// <summary>
+/// Represents a release-track-to-product relationship.
+/// </summary>
+public class ReleaseTrackToProductRelationship
+{
+    /// <summary>
+    /// Gets or sets the release track's number.
+    /// </summary>
+    [Required]
+    public byte TrackNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release media's number.
+    /// </summary>
+    [Required]
+    public byte MediaNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release's unique identifier.
+    /// </summary>
+    [Required]
+    public Guid ReleaseId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the product's unique identifier.
+    /// </summary>
+    [Required]
+    public Guid ProductId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's name.
+    /// </summary>
+    [Required]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the relationship's description.
+    /// </summary>
+    public string? Description { get; set; }
+}
