b7489bf8e492e8d977c32f1a04776a5339f995d3
Andrey Talanin <andrey.talanin@outlook.com>
2023-02-05 01:21:09 +0300

    Add the ReleaseTrackToWorkRelationshipDto database-level entity

---
+++ Source/MusicLibrarySuite.CatalogService.Data/Entities/ReleaseTrackToWorkRelationshipDto.cs
@@ -0,0 +1,52 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicLibrarySuite.CatalogService.Data.Entities;
+
+/// <summary>
+/// Represents a database model and a data transfer object for a release-track-to-work relationship.
+/// </summary>
+public class ReleaseTrackToWorkRelationshipDto
+{
+    /// <summary>
+    /// Gets or sets the release track's number.
+    /// </summary>
+    public byte TrackNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release media's number.
+    /// </summary>
+    public byte MediaNumber { get; set; }
+
+    /// <summary>
+    /// Gets or sets the release's unique identifier.
+    /// </summary>
+    public Guid ReleaseId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the work's unique identifier.
+    /// </summary>
+    public Guid WorkId { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's name.
+    /// </summary>
+    [StringLength(256)]
+    public string Name { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the relationship's description.
+    /// </summary>
+    [StringLength(2048)]
+    public string? Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's display order for the entity acting as a relationship owner (release track).
+    /// </summary>
+    public int Order { get; set; }
+
+    /// <summary>
+    /// Gets or sets the relationship's display order used when requesting relationships for a work.
+    /// </summary>
+    public int ReferenceOrder { get; set; }
+}
